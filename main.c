93452a5b4a83a200bfc9f2bd4308d2a5a4a68606
seandisero <seandisero@gmail.com>
2025-11-21 13:12:16 -0800

    getting things working

--- main.c
+++ main.c
@@ -1,129 +1,331 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE 256
 
 typedef enum {
-	ERR = '\0',
+	TOKEN_NUMBER,
+	TOKEN_OPERATOR,
+	TOKEN_LEFT_BRAKET,
+	TOKEN_RIGHT_BRAKET
+} TokenType;
+
+typedef enum {
 	ADD = '+',
 	SUB = '-',
 	MUL = '*',
-	DIV = '/'
-} operator_type;
+	DIV = '/',
+	POW = '^',
+} OperatorType;
+
+typedef union {
+	OperatorType operator;
+	double number;
+} TokenValue;
 
 typedef struct {
-	int index;
-	operator_type t;
-} operator_t;
+	TokenType type;
+	TokenValue value;
+} Token;
 
 typedef struct {
-	int len;
-	int first;
-	int last;
-	int result;
-	operator_t op;
-	char input[64];
-	char intstr[64];
-} data_t;
+	int size;
+	Token tokens[MAX_SIZE];
+} Stack;
 
-operator_type operator(char c){
-	switch (c){
+typedef struct {
+	int lower;
+	int upper;
+	Token tokens[MAX_SIZE];
+} Queue;
+
+int operator_type_print(OperatorType ot){
+	switch(ot){
 		case ADD:
-			return ADD;
+			printf("ADD");
+			return 0;
 		case SUB:
-			return SUB;
+			printf("SUB");
+			return 0;
 		case MUL:
-			return MUL;
+			printf("MUL");
+			return 0;
 		case DIV:
-			return DIV;
+			printf("DIV");
+			return 0;
+		case POW:
+			printf("POW");
+			return 0;
 	}
-	return ERR;
+	return 1;
 }
 
-int split_by_operator(data_t* data) {
-	int i = 0;
-	char* in = data->input;
-	for (; in[i] != '\0'; i++) {
-		operator_type op = operator(in[i]);
-		if(op != ERR) {
-			data->op.index = i;
-			data->op.t = op;
+int operator_precidence(OperatorType op){
+	switch (op){
+		case ADD:
+			return 2;
+		case SUB:
+			return 2;
+		case MUL:
+			return 3;
+		case DIV:
+			return 3;
+		case POW:
+			return 4;
+	}
+}
+
+int operator_left_associative(OperatorType op){
+	switch (op){
+		case ADD:
 			return 1;
+		case SUB:
+			return 1;
+		case MUL:
+			return 1;
+		case DIV:
+			return 1;
+		case POW:
+			return 0;
+	}
+}
+
+int token_print(Token* t){
+	if(t == NULL){return 1;}
+	if(t->type == TOKEN_NUMBER){
+		printf("num: %f", t->value.number);
+		return 0;
+	}
+	else if(t->type == TOKEN_OPERATOR){
+		printf("operator: ");
+		return operator_type_print(t->value.operator);
+	}
+	else if(t->type == TOKEN_LEFT_BRAKET){
+		printf("braket: left");
+		return 0;
+	}
+	else if(t->type == TOKEN_RIGHT_BRAKET){
+		printf("braket: right");
+		return 0;
+	}
+	return 1;
+}
+
+int stack_init(Stack* s){
+	if(s == NULL) {return 1;}
+	s->size = 0;
+	return 0;
+}
+
+int stack_empty(Stack* s){
+	if(s==NULL){return 1;}
+	if(s->size == 0){return 1;}
+	return 0;
+}
+
+int stack_push(Stack* s, Token* t) {
+	if(s==NULL){return 0;}
+	s->tokens[s->size++] = *t;
+	return 1;
+}
+
+Token* stack_pop(Stack* s) {
+	if(s==NULL){return NULL;}
+	if(stack_empty(s) == 1) {return NULL;}
+	s->size--;
+	return &s->tokens[s->size];
+}
+
+Token* stack_peek(Stack* s){
+	if(s==NULL){return NULL;}
+	if(stack_empty(s) == 1) {return NULL;}
+	return &s->tokens[s->size-1];
+}
+
+void stack_print(Stack* s){
+	char str[MAX_SIZE];
+	for(int i = 0; i < s->size; i++) {
+		token_print(&s->tokens[i]);
+		printf(" ");
+	}
+}
+
+int queue_init(Queue* q){
+	if(q == NULL) {return 1;}
+	q->upper = -1;
+	q->lower = 0;
+	return 0;
+}
+
+int queue_empty(Queue* q){
+	return q->upper < q->lower;
+}
+
+int queue_push(Queue* q, Token* t){
+	if(q == NULL) {return 1;}
+	q->upper++;
+	q->tokens[q->upper] = *t;
+	return 0;
+}
+
+Token* queue_pull(Queue* q){
+	if(q == NULL) {return NULL;}
+	return &q->tokens[q->lower++];
+}
+
+void queue_print(Queue* q){
+	for(int i = q->lower; i <= q->upper; i++) {
+		token_print(&q->tokens[i]);
+		printf("\n");
+	}
+}
+
+int is_num(char c){
+	if(c >= '0' && c <= '9' || c == '.') {return 1;}
+	return 0;
+}
+
+int is_left_braket(char c){
+	return c == '(';
+}
+
+int is_right_braket(char c){
+	return c == ')';
+}
+
+int is_operator(char c){
+	if(c == ADD || c == SUB || c == MUL || c == DIV){
+		return 1;
+	}
+	return 0;
+}
+
+OperatorType char_to_operator(char c){
+	OperatorType o = (OperatorType)c;
+	return o;
+}
+
+int tokenize(Queue*q, char* in) {
+	int current = 0;
+	char str[64];
+	int i;
+	while (in[current] != '\0'){
+		i = 0;
+		Token t;
+		while(in[current] == ' ') {
+			current++;
+			continue;
+		}
+		if(is_num(in[current]) == 1){
+			while(is_num(in[current]) == 1){
+				str[i] = in[current];
+				current++;
+				i++;
+			}
+			str[i] = '\0';
+			double f = atof(str);
+			t.type = TOKEN_NUMBER;
+			t.value.number = f;
+			queue_push(q, &t);
+			continue;
+		}
+		if(is_operator(in[current]) == 1) {
+			str[i] = in[current];
+			t.type = TOKEN_OPERATOR;
+			t.value.operator = char_to_operator(in[current]);
+			current++;
+			queue_push(q, &t);
+			continue;
+		}
+		if(in[current] == '('){
+			t.type = TOKEN_LEFT_BRAKET;
+			current++;
+			queue_push(q, &t);
+			continue;
+		}
+		if(in[current] == ')'){
+			t.type = TOKEN_RIGHT_BRAKET;
+			current++;
+			queue_push(q, &t);
+			continue;
 		}
 	}
 	return 0;
 }
 
-int string_to_int(data_t* data, int lower, int upper) {
-	int i = lower;
-	for (; i < upper; i++) {
-		data->intstr[i - lower] = data->input[i];
-	}
-	data->intstr[i-lower] = '\0';
-	return atoi(data->intstr);
-}
-
-int string_length(char* in){
-	int i = 0;
-	for(; in[i] != '\0' && i < 1000; i++){
-	}
-	return i;
-}
-
-int add(int a, int b){
-	return a + b;
-}
-
-int sub(int a, int b){
-	return a - b;
-}
-
-int mul(int a, int b){
-	return a * b;
-}
-
-int dvi(int a, int b){
-	return a / b;
-}
-
-void handle_input(data_t* data){
-	printf(":");
-	scanf_s("%s", data->input, sizeof(data->input));
-}
-
-int run(data_t* data) {
-	while (1) {
-		handle_input(data);
-		data->len = string_length(data->input); 
-		int success = split_by_operator(data);
-		if(success == 0) {continue;}
-		data->first = string_to_int(data, 0, data->op.index);
-		data->last = string_to_int(data, data->op.index + 1, data->len);
-		switch (data->op.t) {
-			case ADD:
-				data->result = add(data->first, data->last);
-				printf("= %d\n\n", data->result);
-				break;
-			case SUB:
-				data->result = sub(data->first, data->last);
-				printf("= %d\n\n", data->result);
-				break;
-			case MUL:
-				data->result = mul(data->first, data->last);
-				printf("= %d\n\n", data->result);
-				break;
-			case DIV:
-				data->result = dvi(data->first, data->last);
-				printf("= %d\n\n", data->result);
-				break;
-			case ERR:
-				printf("an error occered");
-				break;
-		}
+int run(){
+	char input[MAX_SIZE];
+	Queue q;
+	queue_init(&q);
+	while(1){
+		scanf_s("%s", input);
+		if(strcmp(input, "q") == 0){break;}
+		tokenize(&q, input);
+		printf("stack size: %d", q.upper - q.lower);
+		queue_print(&q);
 	}
 	return 0;
+}
+
+int should_move_operator(Stack* os, Token* t){
+	if(t->type == TOKEN_NUMBER) {return -1;}
+	if(stack_empty(os) == 1) return 0;
+	Token* peek = stack_peek(os);
+	int peek_pr = operator_precidence(peek->value.operator);
+	int t_pr = operator_precidence(t->value.operator);
+	int t_left_ass = operator_left_associative(t->value.operator);
+	return (
+	peek->type != TOKEN_LEFT_BRAKET &&
+	(peek_pr > t_pr || (t_pr == peek_pr && t_left_ass)));
 }
 
 int main() {
-	data_t data;
-	int out = run(&data);
-	exit(out);
+	Queue token_queue;
+	queue_init(&token_queue);
+	char* input = "3.14159 * ( 5.5 + 12.9 )";
+
+	int success = tokenize(&token_queue, input);
+	printf("\n");
+	printf("tokenization success: %d\nnum tokens: %d\n", success, token_queue.upper);
+	printf("%s\n", input);
+	queue_print(&token_queue); printf("\n");
+	printf("\n\n");
+
+	Queue out_queue;
+	Stack operator_stack;
+	success = queue_init(&out_queue);
+	success = stack_init(&operator_stack);
+	printf("queue empty: %d\n", queue_empty(&out_queue));
+
+	for(int i = 0; queue_empty(&token_queue) != 1; i++){
+		Token* t = queue_pull(&token_queue);
+		if(t->type == TOKEN_NUMBER){
+			queue_push(&out_queue, t);
+		}
+		else if (t->type == TOKEN_OPERATOR){
+			while (should_move_operator(&operator_stack, t) == 1){
+				queue_push(&out_queue, stack_pop(&operator_stack));
+			}
+			stack_push(&operator_stack, t);
+		}
+		else if(t->type == TOKEN_LEFT_BRAKET){
+			stack_push(&operator_stack, t);
+		}
+		else if(t->type == TOKEN_RIGHT_BRAKET){
+			while (stack_peek(&operator_stack)->type != TOKEN_LEFT_BRAKET){
+				if(stack_empty(&operator_stack) == 1){
+					return -1;
+				}
+				queue_push(&out_queue, stack_pop(&operator_stack));
+			}
+			Token* discard = stack_pop(&operator_stack);
+		}
+	}
+	while (stack_empty(&operator_stack) != 1){
+		queue_push(&out_queue, stack_pop(&operator_stack));
+	}
+	queue_print(&out_queue); printf(" <- out queue\n");
+
+	return 0;
 }
